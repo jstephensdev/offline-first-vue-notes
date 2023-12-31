5688ef3702a2b27dfe7323626c17c13cd13a3516
“Jessica <jessica.stephens.dev@gmail.com>
2023-12-31 02:44:11 -0600

    using vite env

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,7 +2,7 @@
 
 import { register } from 'register-service-worker'
 
-if (import.meta.env.MODE === 'production') {
+if (import.meta.env.PROD) {
   register(`${import.meta.env.BASE_URL}service-worker.js`, {
     ready () {
       console.log(
